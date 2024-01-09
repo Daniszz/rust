62c93ab9462b86060b9f58ed074e5bf3e65fd9a5
Daniszz <hritcudenis03@gmail.com>
2024-01-09 19:30:14 +0200

    Functia de copiere a fisierului

--- proiect/target/debug/deps/proiect.d
+++ proiect/target/debug/deps/proiect.d
@@ -1,5 +1,5 @@
-c:\Users\hritc\rust\proiect\target\debug\deps\proiect.exe: src\main.rs
+c:\Users\hritc\OneDrive\Documents\GitHub\rust\proiect\target\debug\deps\proiect.exe: src\main.rs
 
-c:\Users\hritc\rust\proiect\target\debug\deps\proiect.d: src\main.rs
+c:\Users\hritc\OneDrive\Documents\GitHub\rust\proiect\target\debug\deps\proiect.d: src\main.rs
 
 src\main.rs:
