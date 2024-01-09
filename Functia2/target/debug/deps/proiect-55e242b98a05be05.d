d12604c8adca105c074531af41f47aafb9416719
Daniszz <126014451+Daniszz@users.noreply.github.com>
2024-01-09 20:08:39 +0200

    FunctiaCopiereStergere

---
+++ Functia2/target/debug/deps/proiect-55e242b98a05be05.d
@@ -0,0 +1,5 @@
+C:\Users\hritc\rust\Functia2\target\debug\deps\proiect-55e242b98a05be05.rmeta: src\main.rs
+
+C:\Users\hritc\rust\Functia2\target\debug\deps\proiect-55e242b98a05be05.d: src\main.rs
+
+src\main.rs:
---
+++ Functia2/target/debug/deps/proiect-a064b53b1cd5efdf.d
@@ -0,0 +1,5 @@
+C:\Users\hritc\rust\Functia2\target\debug\deps\proiect-a064b53b1cd5efdf.rmeta: src\main.rs
+
+C:\Users\hritc\rust\Functia2\target\debug\deps\proiect-a064b53b1cd5efdf.d: src\main.rs
+
+src\main.rs:
